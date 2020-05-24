15c05b3d7b32bc557d398b9fbd0cd687fe912411
Rune Nielsen <runeanielsen@live.dk>
2020-05-24 12:13:06 +0200

    changes color of selected item

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#222222" },
-	[SchemeSel] = { "#ffffff", "#854e9c" },
+	[SchemeSel] = { "#ffffff", "#a30000" },
 	[SchemeOut] = { "#ffffff", "#ffffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
